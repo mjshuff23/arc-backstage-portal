e1baca9a1ac416c129216bc385cbe47f6876ed3a
Michael Shuff <mjshuff23@gmail.com>
2026-07-02 15:17:01 -0400

    gh-issue-128-enable-catalog-and-techdocs-search

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,8 +1,9 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import searchPlugin from '@backstage/plugin-search/alpha';
 import techDocsPlugin from '@backstage/plugin-techdocs/alpha';
 import { navModule } from './modules/nav';
 
 export default createApp({
-  features: [catalogPlugin, techDocsPlugin, navModule],
+  features: [catalogPlugin, searchPlugin, techDocsPlugin, navModule],
 });
